897d395f38fa26bdd4096c70ccd3f82019c29d2e
Daniel Robinson <daniel.robinson.open@gmail.com>
2014-02-23 23:29:45 +0000

    fix mis-type

--- lib/_tran_type.dart
+++ lib/_tran_type.dart
@@ -14,7 +14,7 @@ class _TranType{
   _TranType(String this._key, Type this._type, ToTranString this._toStr, FromTranString this._fromStr){
     _registerAdditionalCoreTypes();
     if(_tranTypesByKey.containsKey(_key)){
-      throw new DuplicateTranKeyType(_key, _type);
+      throw new DuplicateTranKeyError(_key, _type);
     }else if(_tranTypesByType.containsKey(_type)){
       throw new DuplicateTranTypeError(_type, _key);
     }else{
