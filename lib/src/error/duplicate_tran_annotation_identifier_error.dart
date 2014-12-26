ed56780bef5a4e010a465d4034d9bb92727d1638
Daniel Robinson <daniel.robinson.open@gmail.com>
2014-12-26 00:06:17 +0000

    remove incorrectly copy pasted comment

--- lib/src/error/duplicate_tran_annotation_identifier_error.dart
+++ lib/src/error/duplicate_tran_annotation_identifier_error.dart
@@ -4,10 +4,6 @@
 
 part of transmittable;
 
-/// Thrown if the string key used when registering a type has already been used.
-///
-/// This Error should never get thrown, it is left in to help with debugging
-/// if there is a bug in the Transmittable registration algorithm.
 class DuplicatedTranAnnotationIdentifierError extends Error{
   String get message => 'Annotation identifier "identifier" is used more than once in "$fullNamespace".';
   final String fullNamespace;
